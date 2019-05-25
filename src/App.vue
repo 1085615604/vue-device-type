44bdda53ab422fcfd44506cd0672428b5c576fe5
jiayijie <1085615604@qq.com>
2019-05-25 16:07:46 +0800

    create vue-device-type

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+    <device-type :username="nickname"></device-type>
+  </div>
+</template>
+
+<script>
+import deviceType from '../src/myPlugin/getDeviceType/device-type'
+
+export default {
+  name: 'app',
+  components: {
+    deviceType
+  },
+  data () {
+    return {
+      msg: 'home',
+      nickname: '张三'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/myPlugin/getDeviceType/device-type.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="device">
+        hello, {{username}}
+    </div>
+</template>
+
+<script>
+
+export default {
+    components: {
+        
+    },
+    props: {
+        username:{
+            type: String,
+            default: ''
+        }
+    },
+    data () {
+        return {
+            parameter: {}
+        };
+    },
+    computed: {
+    },
+    watch: {},
+    beforeCreate () {
+        
+    },
+    created () {
+        
+    },
+    mounted () {
+        
+        this.isIphoneX = isIphoneX();
+        document.title = '会员权益详情';
+        hsChangeTitle('会员权益详情');
+    },
+    methods: {
+        
+    }
+};
+</script>
+
+<style>
+.device {
+   
+    
+}
+</style>
